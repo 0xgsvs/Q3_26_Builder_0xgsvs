4277e33489afcd6239d58f5aa735728551e6d8b3
0xgsvs <venkat.subrahmanyam.34@gmail.com>
2026-09-09 15:10:10 +0530

    fix(week-2): upgrade litesvm, switch to solana-awesome

--- week_2_assignment/vault/programs/vault/tests/test_initialize.rs
+++ week_2_assignment/vault/programs/vault/tests/test_initialize.rs
@@ -1,31 +1,23 @@
-
-use {
-    anchor_lang::{
-        prelude::Pubkey,
-        solana_program::{instruction::Instruction, system_program},
-        AccountDeserialize, InstructionData, ToAccountMetas,
-    },
-    litesvm::LiteSVM,
-    solana_keypair::Keypair,
-    solana_message::{Message, VersionedMessage},
-    solana_signer::Signer,
-    solana_transaction::versioned::VersionedTransaction,
+use anchor_lang::{
+    AccountDeserialize, InstructionData, ToAccountMetas,
+    prelude::Pubkey,
+    solana_program::{instruction::Instruction, system_program},
+};
+use litesvm::LiteSVM;
+use solana_awesome::{
+    keypair::Keypair,
+    message::{Message, VersionedMessage},
+    signer::Signer,
+    transaction::versioned::VersionedTransaction,
 };
 
 #[test]
 fn test_initialize() {
     let program_id = vault::id();
     let payer = Keypair::new();
-    let counter = Pubkey::find_program_address(
-        &[vault::constants::COUNTER_SEED],
-        &program_id,
-    )
-    .0;
+    let counter = Pubkey::find_program_address(&[vault::constants::COUNTER_SEED], &program_id).0;
     let mut svm = LiteSVM::new();
-    let bytes = include_bytes!(concat!(
-        env!("CARGO_TARGET_TMPDIR"),
-        "/../deploy/vault.so"
-    ));
+    let bytes = include_bytes!(concat!(env!("CARGO_TARGET_TMPDIR"), "/../deploy/vault.so"));
     svm.add_program(program_id, bytes).unwrap();
     svm.airdrop(&payer.pubkey(), 1_000_000_000).unwrap();
 
