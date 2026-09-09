6126ffca93822dde9931abd9615a74c0dba74012
0xgsvs <venkat.subrahmanyam.34@gmail.com>
2026-09-09 11:49:44 +0530

    feat(week-2): scaffold escrow anchor counter program

---
+++ week_2_assignment/escrow/programs/escrow/src/lib.rs
@@ -0,0 +1,25 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("cVefNVDPPmedcognHyK1NQMz1iUJjEmVi4rcHB4AYWu");
+
+#[program]
+pub mod escrow {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        crate::instructions::initialize::handle_initialize(ctx)
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        crate::instructions::increment::handle_increment(ctx)
+    }
+}
